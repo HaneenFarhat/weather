d14e6130cdffb1bacf0f03b6d0ecc1955a8c2433
HaneenFarhat <haneenfarhat.33@gmail.com>
2022-03-18 13:10:25 +0200

    aaaaaaaaaa

---
+++ main.js
@@ -0,0 +1,21 @@
+// const A = async function(){
+
+//     console.log('loading');
+//     setTimeout(( )=>console.log('I should come first, finished loading'),100
+//     )
+    
+// }
+
+
+// const B =function(){
+    
+//     console.log('I should come second');
+// }
+
+
+
+// A()
+// B()
+
+
+<input type="text" id="myInput"></input>
